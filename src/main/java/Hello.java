545af64fc01b8e68fd3c03ce03e27a5d3442f3e6
colocky <callmeadam.ar@gmail.com>
2026-04-13 12:58:26 -0500

    Completed step 2:
    Create these methods:
    a. main()
    b. sayHello() that prints "Hello, World!"
    c. sayGoodbye() that prints "Goodbye!"
    d. sayGoodMorning() that prints "Good morning!"
    e. Call each method from main()

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,24 @@
 public class Hello {
+
+    // main
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+    }
+
+    // Method that prints "Hello world!"
+    public static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+    // Method that prints "Goodbye!"
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+
+    // Method that prints "Good Morning!"
+    public static void sayGoodMorning() {
+        System.out.println("Good Morning!");
+    }
 }
